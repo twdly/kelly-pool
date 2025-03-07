9f1c9ac0da8d2025ba415622b691bbf530d76979
twdly <taiwoodley@gmail.com>
2025-03-07 22:45:35 +1100

    Add selection input for game modes

--- kellypool.client/src/GameSelect/CreateGame.tsx
+++ kellypool.client/src/GameSelect/CreateGame.tsx
@@ -11,9 +11,11 @@ interface CreateGameProps {
 const CreateGame = ({handleCancel, handleCreateGame, handleErrorMessage}: CreateGameProps) => {
     const [gameName, setGameName] = useState<string>('');
     const [playerName, setPlayerName] = useState<string>('');
+    const [mode, setMode] = useState<string>('');
     const playerCountRef = useRef<number>(2);
     
     const playerCounts: number[] = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+    const modes: string[] = ["One other", "Yourself", "Everyone else"];
     
     const createGame = () => {
         handleCreateGame(playerName, gameName, playerCountRef.current)
@@ -37,14 +39,24 @@ const CreateGame = ({handleCancel, handleCreateGame, handleErrorMessage}: Create
 
             <div className={"input-line"}>
                 <label htmlFor={'maxPlayers'}>Max players:</label>
-                <select defaultValue={2}>
+                <select name={'maxPlayers'} defaultValue={2}>
                     {playerCounts.map(n => {
-                        return (<option onClick={() => (playerCountRef.current = n)}>{n}</option>)
+                        return (<option key={n} onClick={() => (playerCountRef.current = n)}>{n}</option>)
                     })}
                 </select>
             </div>
             
-            <button onClick={createGame}>Create game</button>
+            <div className={"input-line"}>
+                <label htmlFor={'modes'}>Known numbers: </label>
+                <select name={'modes'}>
+                    <option onClick={() => setMode('')}>---</option>
+                    {modes.map(m => {
+                        return (<option key={m} onClick={() => setMode(m)}>{m}</option>)
+                    })}
+                </select>
+            </div>
+            
+            <button onClick={createGame} disabled={mode===''}>Create game</button>
         </>
     );
 }
