e70862d59db4f727c0eed6bd1c00e25eeca486b2
Tai <taiwoodley@gmail.com>
2025-05-09 20:47:28 +1000

    Begin creating edit settings page

--- kellypool.client/src/GameView/GameLobbyView.tsx
+++ kellypool.client/src/GameView/GameLobbyView.tsx
@@ -2,6 +2,7 @@ import GameStateModel from "../models/GameStateModel.ts";
 import StateRequestModel from "../models/StateRequestModel.ts";
 import GameStateResponseModel from "../models/GameStateResponseModel.ts";
 import LeaveGameModel from "../models/LeaveGameModel.ts";
+import { useState } from "react";
 
 interface GameLobbyViewProps {
     gameState: GameStateModel,
@@ -12,6 +13,7 @@ interface GameLobbyViewProps {
 
 function GameLobbyView ({gameState, playerId, setGameState, setKnownNumbers}: GameLobbyViewProps) {
     const showWins: boolean = gameState.players.filter((x) => x.wins != 0).length != 0;
+    const [editingSettings, setEditingSettings] = useState<boolean>();
 
     const BeginGame = async () => {
         const beginUri = 'game-state/begin';
@@ -65,24 +67,38 @@ function GameLobbyView ({gameState, playerId, setGameState, setKnownNumbers}: Ga
     }
     
     return (
-        <div>
-            {gameState.gameFinished && (
-                <h3>The winner is: {gameState.winner != null ? `${gameState.winner.name}` : "nobody :("}</h3>
+        <>
+            {editingSettings ? (
+                <>
+                    <button>Save</button>
+                    <button onClick={() => setEditingSettings(false)}>Cancel</button>
+                </>
+            ) : (
+                <div>
+                    {gameState.gameFinished && (
+                        <h3>The winner is: {gameState.winner != null ? `${gameState.winner.name}` : "nobody :("}</h3>
+                    )}
+                    <h2>Players:</h2>
+                    <h3>{gameState.players.length} / {gameState.config.maxPlayers}</h3>
+                    {gameState.players.map((x) => {
+                        return (
+                            <div key={x.id}>
+                                <p>{x.name} {showWins ? `(${x.wins} ${x.wins == 1 ? "win" : "wins"})` : ""}</p>
+                            </div>
+                        )
+                    })}
+                    {gameState.hostId === playerId && (
+                        <>
+                            <button disabled={gameState.players.length == 1} onClick={BeginGame}>Start game</button>
+                            <button onClick={() => setEditingSettings(true)}>Edit settings</button>
+                        </>
+                    )}
+                    <button onClick={HandleLeave}>Leave game</button>
+                </div>
             )}
-            <h2>Players:</h2>
-            <h3>{gameState.players.length} / {gameState.config.maxPlayers}</h3>
-            {gameState.players.map((x) => {
-                return (
-                    <div key={x.id}>
-                        <p>{x.name} {showWins ? `(${x.wins} ${x.wins == 1 ? "win" : "wins"})` : ""}</p>
-                    </div>
-                )
-            })}
-            {gameState.hostId === playerId && (
-                <button disabled={gameState.players.length == 1} onClick={BeginGame}>Start game</button>
-            )}
-            <button onClick={HandleLeave}>Leave game</button>
-        </div>
+        </>
+
+
     );
 }
 
