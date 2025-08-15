687d5f1142884becca4fba36954e5f90d8868324
twdly <taiwoodley@gmail.com>
2025-08-15 18:12:14 +1000

    Simplify Program.cs

--- KellyPool.Server/Program.cs
+++ KellyPool.Server/Program.cs
@@ -1,36 +1,17 @@
 using KellyPool.Server.Services;
 using KellyPool.Server.Services.Interfaces;
 
-namespace KellyPool.Server
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddSingleton<IGamesRepoService, GamesRepoService>();
 
-            builder.Services.AddControllers();
-            builder.Services.AddSingleton<IGamesRepoService, GamesRepoService>();
+var app = builder.Build();
+            
+// Configure the HTTP request pipeline.
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
-            var app = builder.Build();
-
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-
-            // Configure the HTTP request pipeline.
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.MapFallbackToFile("/index.html");
-
-            app.Run();
-        }
-    }
-}
+app.Run();
